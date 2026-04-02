ded85c181b7a9265d80da15026b8787bb613fd3b
Soumya <soumyamohan12106@gmil.com>
2026-04-02 15:23:28 +0530

    Fix email: use Gmail SMTP port 465 with secure true

--- backend/utils/mailer.js
+++ backend/utils/mailer.js
@@ -1,12 +1,13 @@
 import nodemailer from 'nodemailer'
 
 const transporter = nodemailer.createTransport({
-  service: 'gmail',
+  host: 'smtp.gmail.com',
+  port: 465,
+  secure: true,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
   },
-  
 })
 
 export const sendMail = ({ to, subject, html }) =>
